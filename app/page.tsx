87e612e03888ebcd9b3613ea734579cca9df0fc6
Richard V. Narvaez <richard.bvn997@gmail.com>
2024-03-22 13:05:56 -0500

    Add links to business pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import TestComponet from '@/components/TestComponent';
 import { getLang } from '@/lib/lang';
 import { Metadata } from 'next';
+import Link from 'next/link';
 export const metadata: Metadata = {
   title: 'Next 14 LANG Demo',
   description: 'Page for Demo',
@@ -21,6 +22,11 @@ async function Page({ params }: { params: { lang: string } }) {
         <a href="/en" className="border bg-gray-50 rounded py-1 px-4">
           en
         </a>
+      </div>
+
+      <div className='flex gap-3'>
+        <Link href={"/business"}>Business</Link>
+        <Link href={"/business/1231"}>Business with id</Link>
       </div>
     </div>
   );
